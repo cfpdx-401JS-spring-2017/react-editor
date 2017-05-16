b66f4a3d69845cf3dcd5679d5e0f6842042651af
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-16 10:12:04 -0700

    Initial changes to the model, added one setting for a user

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      message: 'Welcome to Hops Music!',
+      color: '#9F0E11'
+    };
+  }
+
+  changeColor(color) {
+    this.setState({ color });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Hops Music: Beta Edition</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="settings">
+          <h3>Let's Play in React: What do you want to do?</h3>
+          <label>
+            Color:
+            <input
+              type="color"
+              value={this.state.color}
+              onChange={e => this.changeColor(e.target.value)} />
+          </label>
+        </div>
+        <div className="display" style={{ color: this.state.color }}>
+          <p className="text">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
